55d13e4b519d089eb238df0a4ee49e3dcbf198b2
Tanya Hasanniazi <niazitanya@gmail.com>
2023-05-08 11:58:58 +0300

    minor_fixes

--- binary_search1.py
+++ binary_search1.py
@@ -1,18 +1,44 @@
 import random
 
-low_input = input('please insert the minimum integer number: ')
-high_input = input('please insert the maximum integer number: ')
 
-#need to put a checker to control if the number is actually integer
-#need to put a checker to control if the max > min 
+#asking the user to insert the minimum and maximum numbers and chech if the values are integer.
+#also in the second while loop we check if the minimum is lower than maximum by at least 2 units.
+while True:
+    low_input = int(input('please insert the minimum integer number: '))
+    try:
+        value_low = int(low_input)
+        break
+    except ValueError:
+        print("Invalid minimum input. Please enter an integer.")
 
-your_number_input = input('please choose an integer number within the range: (I promise I will not see it ;)')
+while True:    
+    high_input = int(input('please insert the maximum integer number: '))
+    try:
+        value_high = int(high_input)
+        while value_high <= value_low + 1:
+            print("Maximum input should be at least 2 units greater than minimum input. Please try again.")
+            high_input = input('please insert the maximum integer number: ')
+            value_high = int(high_input)
+        break
+    except ValueError:
+        print("Invalid maximum input. Please enter an integer.")
 
-low = int(low_input)
-high = int(high_input) + 1
-your_number = int(your_number_input)
 
-def binary_search(low, high):
+while True:    
+    your_number_input = input('please choose an integer number within the range: (I promise I will not see it ;)')
+    try:
+        value = int(your_number_input)
+        while value > value_high or value < value_low:
+            print("your number must be within the range you provided.")
+            your_number_input = input('please choose your number again: ')
+            value = int(your_number_input)
+        break
+    except ValueError:
+        print("Invalid maximum input. Please enter an integer.")
+
+
+
+def binary_search(low, high, your_number):
     guess = random.randint(low, high)
     while True:
         response = input(f'aha! I guess I got it! is your number {guess}? say yes or no, do not lie buddy! ;)')
@@ -43,6 +69,6 @@ def binary_search(low, high):
                 low = guess + 1
                 guess = random.randint(low,high)
 
-binary_search(low,high)
+binary_search(low_input,high_input, your_number_input)
 
 #also the game could be deceived and put to an infinity loop if the player answers the questions wrong (for lower or higher)
